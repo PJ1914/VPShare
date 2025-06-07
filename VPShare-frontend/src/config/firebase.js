dc9e8c21d6c963634ccb79e18abd31184a74f61b
CHINTU PRANAY <pranay.jumbarthi1905@gmail.com>
2025-06-07 18:19:14 +0530

    protection changes

--- VPShare-frontend/src/config/firebase.js
+++ VPShare-frontend/src/config/firebase.js
@@ -1,23 +1,22 @@
 // src/config/firebase.js
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9Tl1vZWWNdXsowXHFdd8eZAv41GDGc-k",
-  authDomain: "vpshare-495ec.firebaseapp.com",
-  projectId: "vpshare-495ec",
-  storageBucket: "vpshare-495ec.appspot.com", 
-  messagingSenderId: "681336563143",
-  appId: "1:681336563143:web:e52a232d9c3856f6ca65e2",
-  measurementId: "G-EMVNJD9BM5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const storage = getStorage(app); 
+const storage = getStorage(app);
 
-export { app, auth, storage, analytics }; 
+export { app, auth, storage, analytics };
